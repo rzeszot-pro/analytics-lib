93544c2b0d4719d82915b995007ccb90b53d3603
Damian Rzeszot <damian.rzeszot@gmail.com>
2019-10-25 21:14:43 +0200

    Add publisher

---
+++ Sources/Analytics/Publisher.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Damian Rzeszot on 24/10/2019.
+//
+
+import Foundation
+
+class Publisher {
+
+    let session = UUID().uuidString.lowercased()
+    var url: URL = URL(string: "https://analytics.rzeszot.pro/api/collector")!
+
+    func publish(data: Data) {
+        print("analytics | publisher | publish")
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = data
+        request.timeoutInterval = 15
+        request.allHTTPHeaderFields = [
+            "content-type": "application/json",
+            "session-id": session
+        ]
+
+        let task = URLSession.shared.dataTask(with: request) { _, _, _ in }
+        task.resume()
+    }
+
+}
